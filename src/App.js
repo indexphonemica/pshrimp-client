71c4dd16f95009653b801e32a80af5414442b1af
defseg <6988161+defseg@users.noreply.github.com>
2019-03-05 00:51:50 -0500

    Add searchbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+const API_URL = 'http://localhost:1337/query/';
+
+function encode(thing) {
+  return encodeURIComponent(thing.replace(/\\/g,'\\\\').replace(/&/g,'\\+').replace(/=/g,'\\e'));
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <SearchForm />
+    );
+  }
+}
+
+class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleData = this.handleData.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const queryURL = API_URL + encode(this.state.value);
+
+    fetch(queryURL, {
+      method: "GET"
+    }).then(function (res) {
+      return res.json();
+    }).then(function (res) {
+      console.log(res);
+    })
+  }
+
+  handleData(event) {
+    event.preventDefault();
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <input type="submit" value="submit" />
+      </form>
     );
   }
 }
