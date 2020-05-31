cf45f3b240de7de17187c0f19681a547969810ab
defseg <6988161+defseg@users.noreply.github.com>
2020-05-30 21:32:50 -0400

    Version for IPHON data release 0.5.0

---
+++ precache-manifest.2eb10487aeef713982f413978a152e2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462a7f1dab9e37afc8b6c64a14f0d4ec",
+    "url": "/pshrimp-client/index.html"
+  },
+  {
+    "revision": "dd7f59939399c7d8ed32",
+    "url": "/pshrimp-client/static/css/2.eea8b61e.chunk.css"
+  },
+  {
+    "revision": "3da2db3a45efdc3967d2",
+    "url": "/pshrimp-client/static/css/main.4f0d47c4.chunk.css"
+  },
+  {
+    "revision": "dd7f59939399c7d8ed32",
+    "url": "/pshrimp-client/static/js/2.284886dc.chunk.js"
+  },
+  {
+    "revision": "96c376a6c3cc45215bb5246c5b720753",
+    "url": "/pshrimp-client/static/js/2.284886dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3da2db3a45efdc3967d2",
+    "url": "/pshrimp-client/static/js/main.667bb2e2.chunk.js"
+  },
+  {
+    "revision": "1aa5c7bd1a5021dc91bf",
+    "url": "/pshrimp-client/static/js/runtime-main.bb5d104f.js"
+  }
+]);
